5c44fb3865563cfbe222273b00d2a8c41785b04e
admin <tanchik776@gmail.com>
2023-01-16 14:22:30 +0300

    hw9

---
+++ src/by/itacademy/hw9/Task1/Main.java
@@ -0,0 +1,21 @@
+package by.itacademy.hw9.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Double firstNumber = 3.14;
+        Integer secondNumber = 2;
+        double result;
+
+        result = Calculator.sum(firstNumber, secondNumber);
+        System.out.println(firstNumber + " + " + secondNumber + " = " + result);
+
+        result = Calculator.multiply(firstNumber, secondNumber);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + result);
+
+        result = Calculator.divide(firstNumber, secondNumber);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + result);
+
+        result = Calculator.subtraction(firstNumber, secondNumber);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + result);
+    }
+}
---
+++ src/by/itacademy/hw9/Task2/Pair.java
@@ -0,0 +1,38 @@
+package by.itacademy.hw9.Task2;
+
+public class Pair<T, J> {
+    private Object first;
+    private Object second;
+
+    public Pair(T first, J second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Object getFirst() {
+        return this.first;
+    }
+
+    public Object getSecond() {
+        return this.second;
+    }
+
+    public void setFirst(Object first) {
+        this.first = first;
+    }
+
+    public void setSecond(Object second) {
+        this.second = second;
+    }
+
+    public void invert() {
+        Object obj = this.first;
+        this.first = this.second;
+        this.second = obj;
+    }
+
+    @Override
+    public String toString() {
+        return first + " & " + second;
+    }
+}
